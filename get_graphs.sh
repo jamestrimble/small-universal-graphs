7da6a8d207e8b7603cbdd61eef7bc170c0ff973d
James Trimble <james.trimble@yahoo.co.uk>
2021-10-10 12:37:00 +0100

    Use geng and gentreeg to generate graphs

--- get_graphs.sh
+++ get_graphs.sh
@@ -2,20 +2,23 @@
 
 set -eu -o pipefail
 
-cd graphs
+mkdir -p graphs
+
+for i in $(seq 1 12); do
+    ./nauty/gentreeg $i > graphs/tree$i.g6
+done
+
+for i in $(seq 1 10); do
+    ./nauty/geng $i > graphs/graph$i.g6
+done
 
 # Copy trees with 1-3 vertices
-cp ../extra-graphs/* .
+cp extra-graphs/* graphs
+
+cd graphs
 
 # Get McKay's lists of trees
 for i in $(seq 4 12); do
     wget https://users.cecs.anu.edu.au/~bdm/data/tree$i.all.tar.gz; tar xvf tree$i.all.tar.gz
     cat tree$i.*.txt > tree$i.all.txt
 done
-
-# Get McKay's lists of graphs
-for i in $(seq 1 9); do
-    wget https://users.cecs.anu.edu.au/~bdm/data/graph$i.g6
-done
-wget https://users.cecs.anu.edu.au/~bdm/data/graph10.g6.gz
-gunzip graph10.g6.gz
