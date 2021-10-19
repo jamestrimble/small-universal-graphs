fd8aa4b29bb23630fb75d6dad561805232466da6
James Trimble <james.trimble@yahoo.co.uk>
2021-10-19 10:51:53 +0100

    Use CPython

--- run_all.sh
+++ run_all.sh
@@ -7,6 +7,6 @@ mkdir results
 
 cat exhaustive_tests.txt | while read type p t; do
     echo $type $p $t
-    pypy3 find_universal_graphs.py $type $p $t --check > results/$type-$p-$t.txt
-    pypy3 graph6_to_dot.py results/$type-$p-$t.txt dot-graphs/$type-$p-$t-{}.dot
+    python3 find_universal_graphs.py $type $p $t --check > results/$type-$p-$t.txt
+    python3 graph6_to_dot.py results/$type-$p-$t.txt dot-graphs/$type-$p-$t-{}.dot
 done
